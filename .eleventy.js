bd0be439df6991b3c5aae73e7685d670241fd6fd
Remi Duval <contact@remiduval.com>
2020-07-01 11:47:14 +0200

    Cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -4,30 +4,16 @@ const svgContents = require("eleventy-plugin-svg-contents");
 module.exports = function (eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
 
-  //eleventyConfig.addWatchTarget("./_tmp/style.css");
-
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("admin");
-  //eleventyConfig.addPassthroughCopy({ "./_tmp/style.css":                         "assets/css/style.css" });
-  eleventyConfig.addPassthroughCopy({ "./node_modules/alpinejs/dist/alpine.js":   "assets/js/alpine.js"});
-  eleventyConfig.addPassthroughCopy({ "content/uploads":                          "assets/uploads" });
-  eleventyConfig.addPassthroughCopy({ "_includes/assets/":                        "assets" });
+  eleventyConfig.addPassthroughCopy({ "content/uploads":    "assets/uploads" });
+  eleventyConfig.addPassthroughCopy({ "_includes/assets/":  "assets" });
 
   eleventyConfig.addPlugin(svgContents);
 
   eleventyConfig.addShortcode("version", function () {
     return String(Date.now());
   });
-
-  /* Markdown Plugins */
-  // let markdownIt = require("markdown-it");
-  // let options = {
-  //   html: true,
-  //   breaks: true,
-  //   linkify: true
-  // };
-  // eleventyConfig.setLibrary("md", markdownIt(options));
-
 
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     if (
@@ -46,10 +32,6 @@ module.exports = function (eleventyConfig) {
     return content;
   });
 
-
-
-
-
   let markdownIt = require("markdown-it");
   let options = {
     breaks: true,
@@ -61,19 +43,6 @@ module.exports = function (eleventyConfig) {
     return mdRender.render(rawString);
   });
 
-
-
-  // var markdownItAttrs = require('markdown-it-attrs');
-  // let options = {
-  //   html: true,
-  //   breaks: true,
-  //   linkify: true
-  // };
-  // let markdownLib = markdownIt(options).use(markdownItAttrs);
-  // eleventyConfig.setLibrary("md", markdownLib);
-
-
-
   return {
     dir: {
       data: "content/globals",
@@ -81,17 +50,5 @@ module.exports = function (eleventyConfig) {
     }
 
   };
-
-
-  // return {
-  //   dir: {
-  //     data: "content/globals",
-  //   },
-  //   templateFormats: ["md", "njk"],
-  //   markdownTemplateEngine: "njk",
-  //   htmlTemplateEngine: "njk",
-  //   passthroughFileCopy: true
-  // };
-
 
 };
